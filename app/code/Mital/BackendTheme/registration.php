9767c8ba34caf6525be6cfb980dd670beb078fca
mitaldeveloper <mitalshah1991@gmail.com>
2021-04-28 13:42:55 +0530

    Backend theme with text translate through module

---
+++ app/code/Mital/BackendTheme/registration.php
@@ -0,0 +1,13 @@
+<?php
+
+\Magento\Framework\Component\ComponentRegistrar::register(
+
+\Magento\Framework\Component\ComponentRegistrar::MODULE,    
+
+'Mital_BackendTheme',
+
+__DIR__
+
+);
+
+?>
---
+++ app/design/adminhtml/Shah/default/registration.php
@@ -0,0 +1,11 @@
+<?php
+
+ \Magento\Framework\Component\ComponentRegistrar::register(
+
+\Magento\Framework\Component\ComponentRegistrar::THEME,
+
+'adminhtml/Shah/default',
+
+__DIR__
+
+);
